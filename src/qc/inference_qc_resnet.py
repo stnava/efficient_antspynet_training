51be62ff210d827c792bde9d1c010599fea448e5
stnava <stnava@gmail.com>
2022-01-19 15:08:13 -0500

    ENH: better example

--- src/qc/inference_qc_resnet.py
+++ src/qc/inference_qc_resnet.py
@@ -58,25 +58,24 @@ t1fn='/Users/stnava/.antspyt1w/ADNI-073_S_4300-20140107-T1w-000.nii.gz'
 t1fn='/Users/stnava/.antspyt1w/28364-00000000-T1w-00.nii.gz'
 t1fn='sub-056_T1wH_v1SR.nii.gz'; dobxt=False
 t1fn='Mindboggle_MMRR-21-5_T1wSRHierarchical_SR.nii.gz'
-t1fn='/Users/stnava/.antspyt1w/28405-00000000-T1w-02.nii.gz'; dobxt=True
 t1fn='/Users/stnava/.antspyt1w/28497-00000000-T1w-04.nii.gz'
 t1fn='/Users/stnava/.antspyt1w/sub-094_T1w_n3.nii.gz'
 t1fn='/Users/stnava/.antspyt1w/28364-00000000-T1w-00nnlow.nii.gz'
 t1fn='/Users/stnava/.antspyt1w/28364-00000000-T1w-00srup.nii.gz'
-t1fn='/Users/stnava/.antspyt1w/28386-00000000-T1w-01.nii.gz'
+t1fn='/Users/stnava/.antspyt1w/28386-00000000-T1w-01.nii.gz' # goood
 t1fn='/Users/stnava/.antspyt1w/28575-00000000-T1w-07.nii.gz'
 t1fn='Landman_1399_20110819_366886505_301_WIP_T1_3D_TFE_iso0_70_SENSE_T1_3D_TFE_iso0_70.nii.gz'; dobxt=True
-t1fn='/Users/stnava/Downloads/eximg.nii.gz' ; dobxt=False
 t1fn='PPMI-107099-20210914-T1wHierarchical-I1498901-SR.nii.gz'; dobxt=False
 t1fn='sub-094-SRHIERbrain_n4_dnz.nii.gz'
 t1fn='PPMI-3810-20110913-MRI_T1-I269587-antspyt1w-V0-brain_n4_dnz-SR.nii.gz'
 t1fn='/Users/stnava/code/code_old/adu/ADNI/reviewData/SRPro/20070820/T1w/000/deep_dkt/ADNI-016_S_0769-20070820-T1w-000-deep_dkt-deep_dkt_Labels-1006-1007-1015-1016_SR.nii.gz'
 t1fn='/Users/stnava/Downloads/temp/basal_forebrain_trt/evaldf/005_S_0929/20061002/T1w/001/antspyt1wsr/V0/ADNI-005_S_0929-20061002-T1w-001-antspyt1wsr-V0-brain_n4_dnz_SR.nii.gz'
 
+dobxt=True
 t1fn='/Users/stnava/.antspyt1w/28575-00000000-T1w-07.nii.gz'
-head=ants.image_read( t1fn )
-insp=antspyt1w.inspect_raw_t1( head, output_prefix='/tmp/XXX' )
-derka
+t1fn='/Users/stnava/Downloads/eximg.nii.gz' ; dobxt=False
+# head=ants.image_read( t1fn )
+t1fn='/Users/stnava/.antspyt1w/28405-00000000-T1w-02.nii.gz'; dobxt=True # bad
 
 x=ants.image_read( t1fn )
 print( t1fn )
@@ -88,5 +87,7 @@ if dobxt:
 else:
     t1 = ants.iMath( x,  "Normalize" )
     bxt = ants.threshold_image( t1, 0.01, 1 )
+
+insp=antspyt1w.inspect_raw_t1( t1*bxt, option='brain', output_prefix='ASS' )
 
 # mygrade = antspyt1w.resnet_grader( x )
